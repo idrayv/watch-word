d5ccab62d41d06fb251a022db444e81595413ce8
idrayv <drayv.mj12@gmail.com>
2017-03-10 12:55:16 +0200

    server.js added

--- WatchWord/gulpfile.js
+++ WatchWord/gulpfile.js
@@ -1,12 +1,17 @@
 ﻿var gulp = require('gulp'),
     Q = require('q'),
+    ts = require('gulp-typescript'),
     rimraf = require('rimraf');
 
-gulp.task('clean', function (cb) {
+gulp.task('clean:lib', function (cb) {
     return rimraf('./wwwroot/lib/', cb);
 });
 
-gulp.task('copy:lib', ['clean'], function () {
+gulp.task('clean:app', function (cb) {
+    return rimraf('./wwwroot/app/', cb);
+});
+
+gulp.task('copy:lib', ['clean:lib'], function () {
     var libs = [
         "@angular",
         "systemjs",
@@ -32,3 +37,13 @@ gulp.task('copy:lib', ['clean'], function () {
 
     return Q.all(promises);
 });
+
+gulp.task('ts', ['clean:app'], function () {
+    var tsProject = ts.createProject('tsconfig.json');
+    var tsResult = gulp.src("Scripts/**/*.ts") // or tsProject.src() 
+        .pipe(tsProject());
+
+    return tsResult.js.pipe(gulp.dest('wwwroot'));
+});
+
+gulp.task('default', ['copy:lib', 'ts']);
