37ac79a6bfd16d699766bd964477f9b2143f2493
idrayv <drayv.mj12@gmail.com>
2017-08-23 11:57:21 +0300

    Implemented: Close translation modal by clicking on background

--- WatchWord/app/global/components/translation-modal/translation-modal.component.ts
+++ WatchWord/app/global/components/translation-modal/translation-modal.component.ts
@@ -1,12 +1,17 @@
-﻿import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+﻿import { Component, OnInit, OnDestroy, Input, ElementRef } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { TranslationModalService } from './translation-modal.service';
 import { ModalService } from '../modal/modal.service';
 import { TranslationModalModel } from './translation-modal.models';
+import Materialssearchmodels = require('../../../materials-search/materials-search.models');
+import RequestStatus = Materialssearchmodels.RequestStatus;
 
 @Component({
     selector: 'translation-modal',
-    templateUrl: 'app/global/components/translation-modal/translation-modal.template.html'
+    templateUrl: 'app/global/components/translation-modal/translation-modal.template.html',
+    host: {
+        '(document:click)': 'documentClick($event.target)'
+    }
 })
 
 export class TranslationModalComponent implements OnInit, OnDestroy {
@@ -14,7 +19,8 @@ export class TranslationModalComponent implements OnInit, OnDestroy {
     private model: TranslationModalModel = new TranslationModalModel();
     private modelSubscription: Subscription;
 
-    constructor(private translationModalService: TranslationModalService, private modalService: ModalService) { }
+    constructor(private translationModalService: TranslationModalService, private modalService: ModalService,
+        private componentElement: ElementRef) { }
 
     public selectTranslation(translation: string): void {
         this.model.wordComposition.vocabWord.translation = translation;
@@ -27,6 +33,12 @@ export class TranslationModalComponent implements OnInit, OnDestroy {
             this.model = model;
             this.modalService.showModal(this.modalId);
         });
+    }
+
+    private documentClick(target: ElementRef): void {
+        if (!this.componentElement.nativeElement.contains(target)) {
+            this.modalService.hideModal(this.modalId);
+        }
     }
 
     ngOnDestroy(): void {
