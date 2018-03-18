34ed483d98c19a01d4f7c2be44c552cfd6ba7a29
Maksym <drayv.mj12@gmail.com>
2018-03-19 00:03:06 +0200

    Heroku server added

---
+++ angular/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 8080);
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
